6d0dc31e13a856217c8d5cc0f199553d7134ccda
Remy D <remyd@civx.us>
2012-02-28 16:27:11 -0500

    Lets actually use the model.template as an example...

--- wsgi/tg2app/tg2app/model/game.py
+++ wsgi/tg2app/tg2app/model/game.py
@@ -1,5 +1,13 @@
+# -*- coding: utf-8 -*-
+"""Sample model module."""
+
 from sqlalchemy import *
-from sqlalchemy.orm import relation, backref
+from sqlalchemy.orm import mapper, relation
+from sqlalchemy import Table, ForeignKey, Column
+from sqlalchemy.types import Integer, Unicode
+#from sqlalchemy.orm import relation, backref
+
+from tg2app.model import DeclarativeBase, metadata, DBSession
 
 
 class Game(DeclarativeBase):
