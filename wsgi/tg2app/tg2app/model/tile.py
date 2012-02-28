f9a3b82193bdc3c0939beece2254c8e3486b6636
Remy D <remyd@civx.us>
2012-02-28 16:30:39 -0500

    Lets use the model.template for tile.py as well...

--- wsgi/tg2app/tg2app/model/tile.py
+++ wsgi/tg2app/tg2app/model/tile.py
@@ -1,5 +1,13 @@
+# -*- coding: utf-8 -*-
+"""Sample model module."""
+
 from sqlalchemy import *
-from sqlalchemy.orm import relation
+from sqlalchemy.orm import mapper, relation
+from sqlalchemy import Table, ForeignKey, Column
+from sqlalchemy.types import Integer, Unicode
+#from sqlalchemy.orm import relation, backref
+
+from tg2app.model import DeclarativeBase, metadata, DBSession
 
 
 class Tile(DeclarativeBase):
@@ -10,3 +18,11 @@ class Tile(DeclarativeBase):
     id = Column(Integer, autoincrement=True, primary_key=True)
     tile_type = Column(Integer)
     game_id = Column(Integer, ForeignKey('game.id'))
+
+    def __init__(self, **kw):
+        """automatically mapping attributes """
+        for key, value in kw.iteritems():
+            setattr(self, key, value)
+
+    def __repr__(self):
+        return "<Game %r>" % (self.id)
